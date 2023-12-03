b846ca3735f6188ff82dd3648feb6b40f1065726
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:09:17 +0100

    Create Monster model

---
+++ Data/Models/Monsters/Monster.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Models.Monsters
+{
+    public class Monster : Entity
+    {
+        public int ExperiencePrize { get; set; }
+        public override Attack AttackAction()
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsStunned { get; set; }
+    }
+}
