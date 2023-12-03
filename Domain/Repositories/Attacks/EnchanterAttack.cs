58ebe197549f4f1c27142d880e1bb030696f5c24
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:12:05 +0100

    Create Enchanter attack method

---
+++ Domain/Repositories/Attacks/EnchanterAttack.cs
@@ -0,0 +1,54 @@
+﻿using Data.Models.Heros;
+using Data.Models.Monsters;
+
+namespace Domain.Repositories.Attacks
+{
+    public static class EnchanterAttack
+    {
+        public static void PerformEnchanterAttack(Enchanter enchanter, Monster monster)
+        {
+            int randomChance = new Random().Next(0, 100);
+
+            if (enchanter.Mana >= 6 && randomChance > 10)
+            {
+                enchanter.Mana -= 6;
+
+                int damage = CalculateEnchanterDamage(enchanter);
+                monster.HealthPoints -= damage;
+
+                if (monster.HealthPoints <= 0)
+                {
+                    enchanter.HealthPoints += enchanter.HealthPoints * 0.25;
+                    enchanter.Mana += 100;
+                    enchanter.Experience += monster.ExperiencePrize;
+                    if (enchanter.Experience > 80)
+                    {
+                        enchanter.Level++;
+                        enchanter.HealthPoints += 10;
+                        enchanter.DamagePoints += 6;
+                    }
+                }
+            }
+            else
+            {
+                if (enchanter.HealthPoints < 30 && enchanter.Mana >= 3 && randomChance <= 10)
+                {
+                    enchanter.Mana -= 10;
+                    enchanter.HealthPoints += 15;
+                }
+                else
+                {
+                    enchanter.Mana += 20;
+                }
+            }
+        }
+
+        private static int CalculateEnchanterDamage(Enchanter enchanter)
+        {
+            int baseDamage = enchanter.DamagePoints;
+            int totalDamage = baseDamage;
+
+            return totalDamage;
+        }
+    }
+}
