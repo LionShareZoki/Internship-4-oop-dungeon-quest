4ce1662b3479c470f685ec32eb2e5a6e3199e63b
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:08:53 +0100

    Create Marksman model

---
+++ Data/Models/Heros/Marksman.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models.Heroes;
+
+namespace Data.Models.Heros
+{
+    public class Marksman : Hero
+    {
+        public int CriticalChance { get; set; }
+        public int StunChance { get; set; }
+
+        public override Attack AttackAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
