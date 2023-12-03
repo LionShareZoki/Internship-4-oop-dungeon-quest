32b1db6009752e5b2712a5c60f447b27e4674c6d
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:08:42 +0100

    Create Gladiator model

---
+++ Data/Models/Heros/Gladiator.cs
@@ -0,0 +1,13 @@
+﻿using Data.Models.Heroes;
+
+namespace Data.Models.Heros
+{
+    public class Gladiator : Hero
+    {
+        public int RageChance { get; set; }
+        public override Attack AttackAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
