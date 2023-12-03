6d8c4150a354b5b14084597b4780bf1ddf67ad70
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:11:23 +0100

    Create Brute Attack method

---
+++ Domain/Repositories/Attacks/BruteAttack.cs
@@ -0,0 +1,20 @@
+﻿
+using Data.Models.Heroes;
+using Data.Models.Monsters;
+
+namespace Domain.Repositories.Attacks
+{
+    internal class BruteAttack
+    {
+        public bool PerformBruteAttack(Hero hero, Brute brute)
+        {
+            var damage = new Random().Next(0, 100) >= 10 ? brute.DamagePoints : hero.HealthPoints * (brute.DamagePoints / 100);
+            hero.HealthPoints -= damage;
+            if (hero.HealthPoints < 0)
+            {
+                return false;
+            }
+            else return true;
+        }
+    }
+}
