a817f41dc6ac2633ddaabd8f9b1f0754325d9273
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:06:38 +0100

    Initial commit

---
+++ Presentation/Classes/ConsoleHelper.cs
@@ -0,0 +1,36 @@
+﻿using Presentation.Interfaces;
+
+namespace Presentation.Repositories
+{
+    public class ConsoleHelper : IConsoleHelper
+    {
+        public int UserInput()
+        {
+            bool isNumber = false;
+            int option;
+            do
+            {
+                Console.WriteLine("Input your option: ");
+                isNumber = Int32.TryParse(Console.ReadLine(), out option);
+            }
+            while (!isNumber);
+
+            return option;
+        }
+
+        public void PrintMenu(Dictionary<int, Action> menuOptions, string menuName)
+        {
+            Console.WriteLine(menuName);
+            foreach (var option in menuOptions)
+            {
+                Console.WriteLine($"{option.Key}");
+            }
+            int menuSelectedOption = UserInput();
+            foreach (var option in menuOptions)
+            {
+                if (option.Key == menuSelectedOption)
+                    option.Value.Invoke();
+            }
+        }
+    }
+}
