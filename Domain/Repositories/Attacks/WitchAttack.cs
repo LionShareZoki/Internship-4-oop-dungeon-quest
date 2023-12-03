dbdb4e91fc22fd7ba8ac980b9cc91a2c48b09b8f
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:11:42 +0100

    Create witch attack method

---
+++ Domain/Repositories/Attacks/WitchAttack.cs
@@ -0,0 +1,21 @@
+﻿
+using Data.Models.Heroes;
+using Data.Models.Monsters;
+
+namespace Domain.Repositories.Attacks
+{
+    internal class WitchAttack
+    {
+        public (bool, bool) PerformWitchAttack(Hero hero, Witch witch)
+        {
+            var damage = new Random().Next(0, 100) >= 10 ? witch.DamagePoints : 0;
+            hero.HealthPoints -= damage;
+            if (hero.HealthPoints < 0)
+            {
+                return (false, false);
+            }
+            else if (damage == 0) return (true, true);
+            else return (true, false);
+        }
+    }
+}
