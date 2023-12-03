4b4f1f1f1c5b2305cc95ac44ef05e93de5c4cc4a
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 19:15:17 +0100

    Create game monitor

---
+++ Presentation/Classes/GameMonitor.cs
@@ -0,0 +1,24 @@
+﻿using Data.Models.Heroes;
+using Data.Models.Monsters;
+using Data.Repositories;
+
+using Domain.Repositories;
+
+namespace Presentation.Classes
+{
+    internal class GameMonitor
+    {
+        public static void GameDataMonitor(Hero hero, List<Monster> monsters)
+        {
+            Console.WriteLine($"{hero.Name}:");
+            Console.WriteLine($"Health points: {hero.HealthPoints}");
+            Console.WriteLine($"Experience: {hero.Experience}");
+            Console.WriteLine($"Level: {hero.Level}");
+
+            Console.WriteLine($"{monsters[0].Name}:");
+            Console.WriteLine($"Health points: {monsters[0].HealthPoints}");
+
+            Console.WriteLine($"There is {monsters.Count} left to fight against.");
+        }
+    }
+}
