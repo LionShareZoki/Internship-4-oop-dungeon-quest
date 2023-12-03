c4d4f300a81e959730f9901779fdd8c55331be87
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:08:31 +0100

    Create Enchanter model

---
+++ Data/Models/Heros/Enchanter.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models.Heroes;
+
+namespace Data.Models.Heros
+{
+    public class Enchanter : Hero
+    {
+        public int Mana { get; set; }
+        public bool HasDied { get; set; }
+
+        public override Attack AttackAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
