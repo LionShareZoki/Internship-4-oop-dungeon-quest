e4ff87e33e2b9a0f27a517457de467d0484a25d6
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 20:52:48 +0100

    Improve Game Monitor UI

--- Presentation/Classes/GameMonitor.cs
+++ Presentation/Classes/GameMonitor.cs
@@ -1,8 +1,5 @@
 ﻿using Data.Models.Heroes;
 using Data.Models.Monsters;
-using Data.Repositories;
-
-using Domain.Repositories;
 
 namespace Presentation.Classes
 {
@@ -11,14 +8,18 @@ namespace Presentation.Classes
         public static void GameDataMonitor(Hero hero, List<Monster> monsters)
         {
             Console.WriteLine($"{hero.Name}:");
-            Console.WriteLine($"Health points: {hero.HealthPoints}");
-            Console.WriteLine($"Experience: {hero.Experience}");
+            Console.WriteLine("");
+            Console.WriteLine($"HP: {hero.HealthPoints}");
+            Console.WriteLine($"Damage: {hero.DamagePoints}");
+            Console.WriteLine($"XP: {hero.Experience}");
             Console.WriteLine($"Level: {hero.Level}");
-
+            Console.WriteLine("------------------");
             Console.WriteLine($"{monsters[0].Name}:");
-            Console.WriteLine($"Health points: {monsters[0].HealthPoints}");
-
+            Console.WriteLine("");
+            Console.WriteLine($"HP: {monsters[0].HealthPoints}");
+            Console.WriteLine($"Damage: {monsters[0].DamagePoints}");
             Console.WriteLine($"There is {monsters.Count} left to fight against.");
+            Console.WriteLine("");
         }
     }
 }
