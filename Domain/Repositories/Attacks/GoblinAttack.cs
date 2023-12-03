0ec65d923f02b24e94ccc751712de9b60512c2da
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:11:35 +0100

    Create goblin attack method

---
+++ Domain/Repositories/Attacks/GoblinAttack.cs
@@ -0,0 +1,19 @@
+﻿
+using Data.Models.Heroes;
+using Data.Models.Monsters;
+
+namespace Domain.Repositories.Attacks
+{
+    internal class GoblinAttack
+    {
+        public bool PerformGoblinAttack(Hero hero, Goblin goblin)
+        {
+            hero.HealthPoints -= goblin.DamagePoints;
+            if (hero.HealthPoints < 0)
+            {
+                return false;
+            }
+            else return true;
+        }
+    }
+}
