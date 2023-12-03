bacc18c31bf6c5b277ed6d98df486b9eb688cde5
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:11:01 +0100

    Create MonsterGenerator

---
+++ Domain/Repositories/MonstersGenerator.cs
@@ -0,0 +1,74 @@
+﻿using Data.Enums;
+using Data.Models.Monsters;
+
+namespace Domain.Repositories
+{
+    public class MonstersGenerator
+    {
+        public List<Monster> GenerateRandomMonsters(int count)
+        {
+            List<Monster> monsters = new List<Monster>();
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                MonstersType monsterType = DetermineMonsterType(random.Next(1, 101));
+
+                Monster monster = GenerateMonster(monsterType);
+                monsters.Insert(0, monster);
+            }
+
+            return monsters;
+        }
+
+        private MonstersType DetermineMonsterType(int randomNumber)
+        {
+            if (randomNumber <= 60)
+            {
+                return MonstersType.Goblin;
+            }
+            else if (randomNumber <= 90)
+            {
+                return MonstersType.Brute;
+            }
+            else
+            {
+                return MonstersType.Witch;
+            }
+        }
+
+        private Monster GenerateMonster(MonstersType monsterType)
+        {
+            switch (monsterType)
+            {
+                case MonstersType.Brute:
+                    return new Brute
+                    {
+                        PercentageAttackChance = 20,
+                        DamagePoints = 15,
+                        HealthPoints = 20,
+                        ExperiencePrize = 15
+                    };
+                case MonstersType.Goblin:
+                    return new Goblin
+                    {
+                        SpecialAbilityChance = 0,
+                        DamagePoints = 5,
+                        HealthPoints = 20,
+                        ExperiencePrize = 10
+                    };
+                case MonstersType.Witch:
+                    return new Witch
+                    {
+                        DamagePoints = 22,
+                        HealthPoints = 30,
+                        ExperiencePrize = 20,
+                        HasDied = false,
+                        SpecialAbilityChance = 10
+                    };
+                default:
+                    throw new ArgumentException("Invalid monster type.");
+            }
+        }
+    }
+}
