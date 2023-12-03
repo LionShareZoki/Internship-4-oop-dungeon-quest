1b181e94de38d48983494ac8387b0d71f65aa25b
LionShareZoki <pujic.zoran@gmail.com>
2023-12-03 12:13:19 +0100

    Create MonstersGeneratorConsole

---
+++ Presentation/Classes/MonstersGeneratorConsole.cs
@@ -0,0 +1,57 @@
+﻿using Data.Models.Monsters;
+
+using Domain.Repositories;
+
+using Presentation.Repositories;
+
+internal class MonsterConsole
+{
+    private readonly ConsoleHelper consoleHelper;
+    private readonly MonstersGenerator monstersGenerator;
+
+    public MonsterConsole()
+    {
+        consoleHelper = new ConsoleHelper();
+        monstersGenerator = new MonstersGenerator();
+    }
+
+    public void RunMonsterConsole()
+    {
+        Console.WriteLine("Are you ready to see who you are fighting against? (Y/N)");
+        if (Console.ReadLine()?.ToUpper() == "Y")
+        {
+            GenerateMonsters();
+        }
+    }
+
+    private void GenerateMonsters()
+    {
+        List<Monster> monsters = monstersGenerator.GenerateRandomMonsters(10);
+
+        Console.WriteLine("Generated 10 monsters:");
+
+        int bruteCount = 0;
+        int goblinCount = 0;
+        int witchCount = 0;
+
+        foreach (var monster in monsters)
+        {
+            if (monster is Brute)
+            {
+                bruteCount++;
+            }
+            else if (monster is Goblin)
+            {
+                goblinCount++;
+            }
+            else if (monster is Witch)
+            {
+                witchCount++;
+            }
+        }
+
+        Console.WriteLine($"Brute Count: {bruteCount}");
+        Console.WriteLine($"Goblin Count: {goblinCount}");
+        Console.WriteLine($"Witch Count: {witchCount}");
+    }
+}
