ff4bb081863d300037a2322c8ed2713026affa64
LionShareZoki <pujic.zoran@gmail.com>
2023-12-04 18:38:54 +0100

    Add Buy HP method

---
+++ Domain/Repositories/BuyHealthPoints.cs
@@ -0,0 +1,15 @@
+﻿using Data.Models.Heroes;
+
+namespace Domain.Repositories
+{
+    public class BuyHP(Hero hero)
+    {
+        public void BuyHealthPoints(Hero hero)
+        {
+            hero.Experience /= 2;
+            hero.HealthPoints = hero.MaxHealthPoints;
+            Console.WriteLine("Successful transaction.");
+            Console.ReadKey();
+        }
+    }
+}
